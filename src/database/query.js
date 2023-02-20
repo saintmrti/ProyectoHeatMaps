922f0b19a912aa44c6f77b4b5d2a7278daa8e372
proyectosNuvahub <nuvahub@outlook.com>
2023-02-20 12:25:55 -0600

    cambios en index

--- src/database/query.js
+++ src/database/query.js
@@ -5,24 +5,24 @@ export const getDataArtic = async (res, req) => {
         const pool = await getConnection();
         const result1 = await pool.request().query(`
             SELECT * FROM Nuitrack_Frames 
-            WHERE fecha  >= '2023-02-10 09:00:00' AND fecha <='2023-02-10 10:00:00'
+            WHERE fecha  >= '2023-02-08 00:01:00' AND fecha <='2023-02-08 06:00:00'
         `);
-        // const result2 = await pool.request().query(`
-        //     SELECT * FROM Nuitrack_Frames 
-        //     WHERE fecha  >= '2023-02-03 06:01:00' AND fecha <='2023-02-03 12:00:00'
-        // `);
-        // const result3 = await pool.request().query(`
-        //     SELECT * FROM Nuitrack_Frames 
-        //     WHERE fecha  >= '2023-02-03 12:01:00' AND fecha <='2023-02-03 18:00:00'
-        // `);
-        // const result4 = await pool.request().query(`
-        //     SELECT * FROM Nuitrack_Frames 
-        //     WHERE fecha  >= '2023-02-03 18:01:00' AND fecha <='2023-02-03 23:59:00'
-        // `);
+        const result2 = await pool.request().query(`
+            SELECT * FROM Nuitrack_Frames 
+            WHERE fecha  >= '2023-02-08 06:01:00' AND fecha <='2023-02-08 12:00:00'
+        `);
+        const result3 = await pool.request().query(`
+            SELECT * FROM Nuitrack_Frames 
+            WHERE fecha  >= '2023-02-08 12:01:00' AND fecha <='2023-02-08 18:00:00'
+        `);
+        const result4 = await pool.request().query(`
+            SELECT * FROM Nuitrack_Frames 
+            WHERE fecha  >= '2023-02-08 18:01:00' AND fecha <='2023-02-08 23:59:00'
+        `);
 
-        // const data= [...result1.recordset, ...result2.recordset, ...result3.recordset, ...result4.recordset];
+        const data= [...result1.recordset, ...result2.recordset, ...result3.recordset, ...result4.recordset];
 
-        return result1.recordset;
+        return data;
 
     } catch (error) {
         console.log(error);
