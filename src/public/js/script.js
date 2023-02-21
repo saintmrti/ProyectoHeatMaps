4bd7505f24947c32e0d1c216c5271f39cda708e0
proyectosNuvahub <nuvahub@outlook.com>
2023-02-21 15:58:27 -0600

    hicimos modificaciones en el index.html y el script.js para acomodar mejor el mapa de calor

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,9 +1,62 @@
+fetch('/miercoles')
+.then(response => response.json())
+.then(data => {
+    function update() {
+        const container = document.getElementById('heatmap1');
+        const containerRect = container.getBoundingClientRect();
+        const width = containerRect.width;
+        const height = containerRect.height;
+
+        const margin = { top: 20, right: 0, bottom: 30, left: 0  };
+
+        console.log(width);
+        console.log(height);
+
+        const maxX = d3.max(data, d => d.coord[0]);
+        const maxY = d3.max(data, d => d.coord[1]);
+
+        const xScale = d3.scaleLinear()
+        .domain([0, maxX])
+        .range([margin.left, width - margin.right]);
+
+        const yScale = d3.scaleLinear()
+        .domain([0, maxY])
+        .range([height - margin.bottom, margin.top]);
+
+        const colorScale = d3.scaleLinear()
+        .domain([0, 0.509 / 2, 0.509])
+        .range(["lightgreen", "orange", "red"])
+        .interpolate(d3.interpolateRgb);
+
+        // console.log(d3.max(data, d => d.count));
+
+        const svg = d3.select('#heatmap1')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('viewBox', `0 0 ${width} 500`)
+        .style("transform", "rotate(180deg)");
+
+        svg.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('cx', d => xScale(d.coord[0]))
+        .attr('cy', d => yScale(d.coord[1]))
+        .attr('r', 5)
+        .attr('fill', d => colorScale(d.count));
+    }
+
+    update();
+    window.addEventListener('resize', update);
+});
+
 fetch('/viernes')
 .then(response => response.json())
 .then(data => {
     const width = 900;
     const height = 900;
-    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const margin = { top: 20, right: 0, bottom: 30, left: 0  };
 
     const xScale = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.coord[0])])
@@ -14,14 +67,13 @@ fetch('/viernes')
     .range([height - margin.bottom, margin.top]);
 
     const colorScale = d3.scaleLinear()
-    .domain([-2.261, 0, 2.261 / 2, 2.261])
-    .range(['blue', 'lightgreen', 'orange', 'red'])
+    .domain([0, 0.509 / 2, 0.509])
+    .range(["lightgreen", "orange", "red"])
     .interpolate(d3.interpolateRgb);
 
-    console.log(d3.min(data, d => d.count));
     console.log(d3.max(data, d => d.count));
 
-    const svg = d3.select('.heatmap')
+    const svg = d3.select('#heatmap2')
     .append('svg')
     .attr('width', width)
     .attr('height', height);
@@ -35,3 +87,42 @@ fetch('/viernes')
     .attr('r', 5)
     .attr('fill', d => colorScale(d.count));
 });
+
+fetch('/diferencia')
+.then(response => response.json())
+.then(data => {
+    const width = 900;
+    const height = 900;
+    const margin = { top: 20, right: 0, bottom: 30, left: 0  };
+
+    const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.coord[0])])
+    .range([margin.left, width - margin.right]);
+
+    const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.coord[1])])
+    .range([height - margin.bottom, margin.top]);
+
+    const colorScale = d3.scaleLinear()
+    .domain([-0.509, 0, 0.509 / 2, 0.509])
+    .range(['blue', 'lightgreen', 'orange', 'red'])
+    .interpolate(d3.interpolateRgb);
+
+    console.log(d3.min(data, d => d.count));
+    console.log(d3.max(data, d => d.count));
+
+    const svg = d3.select('#heatmap3')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+    svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', d => xScale(d.coord[0]))
+    .attr('cy', d => yScale(d.coord[1]))
+    .attr('r', 5)
+    .attr('fill', d => colorScale(d.count));
+});
+
